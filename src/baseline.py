31b145a2bf20cb764dba508695d3593db69c3939
Mohid Tanveer <mohid.tanveer011@gmail.com>
2025-11-29 15:25:07 -0600

    wording

--- src/baseline.py
+++ src/baseline.py
@@ -1,16 +1,10 @@
 def compute_global_average(ratings):
-    # avoid div by zero when we have no ratings
-    if not ratings:
-        return 0.0
     return sum(rating for _, _, rating in ratings) / len(ratings)
 
 
 def global_average_mse(ratings_train, ratings_valid):
     # return mse when predicting the global average everywhere
     global_avg = compute_global_average(ratings_train)
-
-    if not ratings_valid:
-        return 0.0, global_avg
 
     mse = sum((global_avg - rating) ** 2 for _, _, rating in ratings_valid) / len(
         ratings_valid
