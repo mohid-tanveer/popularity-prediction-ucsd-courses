07a02a9d053692cc5a271a3e9349342f262ca7b3
Mohid Tanveer <mohid.tanveer011@gmail.com>
2025-11-29 13:44:59 -0600

    cleanup

--- src/baseline.py
+++ src/baseline.py
@@ -1,3 +1,14 @@
+def getGlobalAverage(trainRatings):
+    # return the average rating in the training set
+    return sum(trainRatings) / len(trainRatings)
+
+
+def trivialValidMSE(ratingsValid, globalAverage):
+    return sum((rating[2] - globalAverage) ** 2 for rating in ratingsValid) / len(
+        ratingsValid
+    )
+
+
 def baseline():
     # a baseline model which considers global avg; user-bias (professor) and item-bias(course code)
     pass
