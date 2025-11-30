cb074f30dee3b0ebda3439f0138296dbd56df5fe
AlbertChen7 <alchen1818@gmail.com>
2025-11-29 19:52:27 -0800

    Added alpha parameter for ridge model func

--- src/models/tfidf.py
+++ src/models/tfidf.py
@@ -5,14 +5,14 @@ from sklearn.linear_model import Ridge
 # tfidf residual regression                      #
 ##################################################
 
-def tfidfRidgeReg(ratingsTrainWithTFIDF, preds):
+def tfidfRidgeReg(ratingsTrainWithTFIDF, preds, a=1.0):
     yTrain = np.array([r[-1] for r in ratingsTrainWithTFIDF])
     residuals = yTrain - preds
     
     # get just the TFIDF vectors
     XTrainTFIDF = [r[2:-1] for r in ratingsTrainWithTFIDF]
     
-    ridge = Ridge(alpha=1.0)
+    ridge = Ridge(alpha=a)
     ridge.fit(XTrainTFIDF, residuals)
     
     return ridge
